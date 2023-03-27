eb1d96fd7eb8215302e938124bca2b4887c1eda3
Suryash <joblesscoder2468@gmail.com>
2023-03-27 23:02:01 +0530

    Number plate text validation done

--- plateDetection/views.py
+++ plateDetection/views.py
@@ -8,6 +8,12 @@ import time
 # Create your views here.
 
 
+def appendToTxt(currTime):
+    f = open("static/txt/checked.txt", "a")
+    f.write(str(currTime)+'\n')
+    f.close()
+
+
 def extractAndSaveNumberPlate(currTime):
     # Read the image in BGR
     fileName = f'static/img/recvd/recvd_{currTime}.jpg'
@@ -21,43 +27,69 @@ def extractAndSaveNumberPlate(currTime):
 
     def carplate_detect(image):
         carplate_overlay = image.copy()
+        # image resizing
+        resize_test_NP = cv2.resize(
+            carplate_overlay, None, fx=2, fy=2,
+            interpolation=cv2.INTER_CUBIC)
+
+        # converting image to grayscale
+        grayscale_resize_test_NP = cv2.cvtColor(
+            resize_test_NP, cv2.COLOR_BGR2GRAY)
+
+        # denoising the image
+        gaussian_blur_NP = cv2.GaussianBlur(
+            grayscale_resize_test_NP, (5, 5), 0)
+
+        fileNameX = f'static/img/clear/clear_{currTime}.jpg'
+        cv2.imwrite(fileNameX, gaussian_blur_NP)
+
+        # cv2.imshow("testIt", gaussian_blur_NP)
+        # cv2.waitKey(0)
+
+        # temproary fix
+        # carplate_rects = carplate_haar_cascade.detectMultiScale(
+        # gaussian_blur_NP, scaleFactor=1.01, minNeighbors=5)
+
         carplate_rects = carplate_haar_cascade.detectMultiScale(
             carplate_overlay, scaleFactor=1.1, minNeighbors=3)
-
+        # print(carplate_rects)
         for x, y, w, h in carplate_rects:
             # save image at location
+            print(x, y, w, h)
             ROI = carplate_overlay[y:y+h, x:x+w]
             fileName = f'static/img/extracted/extracted_{currTime}.png'
             cv2.imwrite(fileName, ROI)
             # cv2.rectangle(carplate_overlay, (x, y),
             #   (x+w, y+h), (255, 255, 0), 5)
+            appendToTxt(currTime)
 
         return carplate_overlay
-    f = open("static/txt/checked.txt", "w+")
-    f.write(str(currTime))
-    f.close()
     detected_carplate_img = carplate_detect(carplate_img_rgb)
 
 
 def home(request):
+    try:
 
-    # get epoch time in milliseconds
-    currTime = int(round(time.time() * 1000))
-    # print(currTime)
-    print(request.POST)
-    if request.method == "POST":
-        # print(request.FILES)
-        data = request.FILES['mainImg']
-        fileName = f'static/img/recvd/recvd_{currTime}.jpg'
-        with open(fileName, 'wb') as f:
-            image = Image.open(data)
-            image.save(f, format='JPEG')
+        # get epoch time in milliseconds
+        currTime = int(round(time.time() * 1000))
+        # print(currTime)
+        print(request.POST)
+        if request.method == "POST":
+            # print(request.FILES)
+            data = request.FILES['mainImg']
+            fileName = f'static/img/recvd/recvd_{currTime}.jpg'
+            with open(fileName, 'wb') as f:
+                image = Image.open(data)
+                image.save(f, format='JPEG')
 
-    # to repair django
-    extractAndSaveNumberPlate(1679797086300)
-
-    # extractAndSaveNumberPlate(currTime)
-    return render(request, "plateDetection/indexr.html")
+        # to repair django
+        # extractAndSaveNumberPlate(1679927492294)
+        print(currTime)
+        extractAndSaveNumberPlate(currTime)
+        return render(request, "plateDetection/indexr.html")
+    except Exception as e:
+        print(e)
+        return render(request, "plateDetection/indexr.html")
 
 
 def tempResult(request):
--- static/js/tesseract-ocr.js
+++ static/js/tesseract-ocr.js
@@ -58,44 +58,57 @@ function startRecognize(img) {
 	recognizeFile(img);
 }
 
-function progressUpdate(packet) {
-	var log = document.getElementById('log');
+// function progressUpdate(packet) {
+// 	var log = document.getElementById('log');
 
-	if (log.firstChild && log.firstChild.status === packet.status) {
-		if ('progress' in packet) {
-			var progress = log.firstChild.querySelector('progress')
-			progress.value = packet.progress
+// 	if (log.firstChild && log.firstChild.status === packet.status) {
+// 		if ('progress' in packet) {
+// 			var progress = log.firstChild.querySelector('progress')
+// 			progress.value = packet.progress
+// 		}
+// 	} else {
+// 		var line = document.createElement('div');
+// 		line.status = packet.status;
+// 		var status = document.createElement('div')
+// 		status.className = 'status'
+// 		// status.appendChild(document.createTextNode(packet.status))
+// 		// line.appendChild(status)
+
+// 		if ('progress' in packet) {
+// 			var progress = document.createElement('progress')
+// 			progress.value = packet.progress
+// 			progress.max = 1
+// 			// line.appendChild(progress)
+// 		}
+
+
+// 		if (packet.status == 'done') {
+// 			log.innerHTML = ''
+// 			var pre = document.createElement('pre')
+// 			pre.appendChild(document.createTextNode(packet.data.text.replace(/\n\s*\n/g, '\n')))
+// 			line.innerHTML = ''
+// 			line.appendChild(pre)
+// 			$(".fas").removeClass('fa-spinner fa-spin')
+// 			$(".fas").addClass('fa-check')
+// 		}
+
+// 		log.insertBefore(line, log.firstChild)
+// 	}
+// }
+
+
+function cleanResult(text) {
+	var res = '';
+	for (var i = 0; i < text.length; i++) {
+		if ((text[i].charCodeAt(0) >= 65 && text[i].charCodeAt(0) <= 90) || (text[i].charCodeAt(0) >= 97 && text[i].charCodeAt(0) <= 122) || (text[i].charCodeAt(0) >= 48 && text[i].charCodeAt(0) <= 57)) {
+			res += text[i];
 		}
-	} else {
-		var line = document.createElement('div');
-		line.status = packet.status;
-		var status = document.createElement('div')
-		status.className = 'status'
-		// status.appendChild(document.createTextNode(packet.status))
-		// line.appendChild(status)
+		// else console.log(text[i]);
 
-		if ('progress' in packet) {
-			var progress = document.createElement('progress')
-			progress.value = packet.progress
-			progress.max = 1
-			// line.appendChild(progress)
-		}
-
-
-		if (packet.status == 'done') {
-			log.innerHTML = ''
-			var pre = document.createElement('pre')
-			pre.appendChild(document.createTextNode(packet.data.text.replace(/\n\s*\n/g, '\n')))
-			line.innerHTML = ''
-			line.appendChild(pre)
-			$(".fas").removeClass('fa-spinner fa-spin')
-			$(".fas").addClass('fa-check')
-		}
-
-		log.insertBefore(line, log.firstChild)
 	}
+	// console.log(res);
+	return res;
 }
-
 function recognizeFile(file) {
 	$("#log").empty();
 	const corePath = window.navigator.userAgent.indexOf("Edge") > -1
@@ -110,13 +123,16 @@ function recognizeFile(file) {
 	worker.recognize(file,
 		$("#langsel").val()
 	)
-		.progress(function (packet) {
-			console.info(packet)
-			// progressUpdate(packet)
+		// .progress(function (packet) {
+		// 	console.info(packet)
+		// 	// progressUpdate(packet)
 
-		})
+		// })
 		.then(function (data) {
 			console.log(data)
-			progressUpdate({ status: 'done', data: data })
+			console.log("Output through OCR", data.text)
+			// console.log()
+			document.getElementById('startPre').innerHTML = cleanResult(data.text);
+			// progressUpdate({ status: 'done', data: data })
 		})
 }
